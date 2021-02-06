26db01c77379e93b51b911292248cb0e8c3de937
Okute Wonah <okutewonah@gmail.com>
2021-02-06 07:15:59 +0200

    ex5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Okute Wonah'
+my_age = 32 # true story
+my_height = 190 # cm, true story
+my_weight = 71 # kg, awful
+my_eyes = 'Brown'
+my_teeth = 'White' # I guess
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He is {my_weight} kg heavy.")
+print("Actually that's really light")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight}, I get {total}.")
